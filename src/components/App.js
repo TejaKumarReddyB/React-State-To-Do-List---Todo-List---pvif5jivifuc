26e69f9e2385895c056b7c9a4862d4f409cdc993
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-06-26 23:27:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -51,8 +51,6 @@ const uplists=lists.filter((ele)=> ele.id!==ind)
 
 	return ( 
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
 	{ 
 	toggle?
 	<>
